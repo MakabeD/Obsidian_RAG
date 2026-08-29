d49e8addfc2e4a40e84a3a7e25406c19a3a3c4d0
Daniel Yepes <danielcoc356@gmail.com>
2026-08-28 23:32:26 -0500

    add singleton in "md" Post. And clean end point

--- Program.cs
+++ Program.cs
@@ -3,19 +3,19 @@ using chunker;
 using vaultReader;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton(
+    new EmbeddingService("model/model.onnx", "model/vocab.txt"));
+
 var app = builder.Build();
 
 
 
-app.MapPost("/md", async (HttpRequest request) =>
+app.MapPost("/md", async (EmbeddingService embed, HttpRequest request) =>
 {
     var form = await request.ReadFormAsync();
     
     List<DocumentData> Documents = await VaultReader.reader(form.Files);
 
-    var x = Chunker.Chunking(Documents[0], 600);
-    var embed=new EmbeddingService("model/model.onnx", "model/vocab.txt");
-    
     return Results.Ok(embed.Embed(Documents[0].Content));
 })
 .DisableAntiforgery();
