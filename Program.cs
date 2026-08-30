3a6191e1b1403f632884285526653d63ddcf6bde
Daniel <danielcoc356@gmail.com>
2026-08-30 05:05:21 +0000

    Adds http client to builder.Services

--- Program.cs
+++ Program.cs
@@ -6,19 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton(
     new EmbeddingService("model/model.onnx", "model/vocab.txt"));
 
+builder.Services.AddHttpClient<ChromaService>(c =>
+    c.BaseAddress = new Uri("http://localhost:8000"));
+
 var app = builder.Build();
 
 
 
-app.MapPost("/md", async (EmbeddingService embed, HttpRequest request) =>
+app.MapPost("/md", async (EmbeddingService embed, ChromaService chroma, HttpRequest request) =>
 {
     var form = await request.ReadFormAsync();
-    
+
     List<DocumentData> Documents = await VaultReader.reader(form.Files);
     IEnumerable<DocumentChunk> chunks = Chunker.Chunking(Documents, 600);
-    HttpClient a= new HttpClient{BaseAddress= new Uri("http://localhost:8000")};
-    ChromaService service= new ChromaService(a);
-    await service.InitializeAsync();
+    await chroma.InitializeAsync();
     return Results.Ok(embed.EmbeddRange(chunks));
 })
 .DisableAntiforgery();
